d599cc9b48f80b01affbb8483727c810900c2a12
Dave O'Neill <dmo@roaringpenguin.com>
2010-03-01 15:43:01 -0500

    Reformat for readability

--- wind-towers.pl
+++ wind-towers.pl
@@ -3,68 +3,19 @@ use strict;
 use warnings;
 use LWP::Simple;
 use Data::Dumper;
-use 5.010;
-use Switch;
 
 # http://sd.ic.gc.ca/pls/engdoc_anon/web_search.licensee_name_results?output_format=2&selected_columns=TX_FREQ,RX_FREQ,LOCATION,COMPANY_NAME&col_in_fmt=COMMA_LIST&selected_column_group=NONE&extra_ascii=LINK_STATION&extra_xml=None&licensee_name=Globalive%20Wireless%20Management%20Corp.%20Attn%3A%20Ahmed%20Derini&admin_do=41&company_cd=90045300
 # or shorter:
 # http://sd.ic.gc.ca/pls/engdoc_anon/web_search.licensee_name_results?output_format=2&selected_columns=TX_FREQ,RX_FREQ,LOCATION&col_in_fmt=COMMA_LIST&selected_column_group=NONE&extra_ascii=LINK_STATION&admin_do=41&company_cd=90045300
 
-# Grab content
-# my $content = do { open(my $fh, "<raw_data.txt") or die "No data!";  local $/; <$fh> };
+my $ontario_search_url = 'http://sd.ic.gc.ca/pls/engdoc_anon/web_search.licensee_name_results?output_format=2&selected_columns=TX_FREQ,RX_FREQ,LOCATION&col_in_fmt=COMMA_LIST&selected_column_group=NONE&extra_ascii=LINK_STATION&admin_do=41&company_cd=90045300';
 
-my $content = get('http://sd.ic.gc.ca/pls/engdoc_anon/web_search.licensee_name_results?output_format=2&selected_columns=TX_FREQ,RX_FREQ,LOCATION&col_in_fmt=COMMA_LIST&selected_column_group=NONE&extra_ascii=LINK_STATION&admin_do=41&company_cd=90045300');
-die "Couldn't fetch content" unless defined $content;
-
-$content =~ s/\r\n/\n/g;
-
-# Extract legend and data
-my ($raw_legend) = $content =~ m/Field Position Legend(.*)/sm;
-my $legend = [];
-foreach my $line (split(/\n/, $raw_legend)) {
-	my ($name, $start, $end) = $line =~ m/(.*?)\s+(\d+) - (\d+)/;
-	next unless $name;
-
-	$name =~ s/\s+$//;
-
-	# Pull off units
-	my $units = undef;
-	if( $name =~ m/\((.*?)\)$/ ) {
-		$units = $1;
-	}
-
-	my $key = $name;
-	$key =~ s/\(.*?\)//g;
-	$key =~ s/\s+$//;
-	$key =~ s/\s+/_/g;
-
-	my $col = {
-		key   => $key,
-		units => $units,
-		name  => $name,
-		start => $start - 1,
-		len   => $end - $start + 1,
-	};
-	push @$legend,$col;
-
-}
-
-my $regex   = join('\s', map { "(.{$_->{len},$_->{len}})" } @$legend );
-my @key_ary = map { $_->{key} } @$legend;
-
-my ($data)   = $content =~ m/\[DATA\](.*)\[\/DATA\]/sm;
-my @rows;
-foreach my $line (split(/\n/,$data)) {
-	my (@tmprow) = $line =~ /$regex/o;
-
-	my %row;
-
-	@tmprow = map { s/^\s+//; $_ } @tmprow;
-	@row{@key_ary} = map { s/\s+$//; $_ } @tmprow;
-	push @rows, \%row;
-}
+my $content   = retrieve_content( $ontario_search_url );
+my $legend    = extract_legend( $content );
+my $data_rows = extract_data( $legend, $content );
 
 # Yes, the misspellings do exist in the source data.  Industry Canada needs to hire some geography students.
+# Incorrect spellings left in ALL CAPS as in source data, to make them easier to remove later.
 my %metro_areas = (
 	ottawa => [qw(
 		Ottawa Kanata Nepean Gatineau
@@ -72,23 +23,22 @@ my %metro_areas = (
 	)],
 
 	gta   => [qw(
-		Toronto Ajax Oshawa Etobicoke Burlington Oakville Brampton Mississauga Hamilton Whitby Scarborough
-		Vaughan Markham Milton Aurora Downsview Rexdale Thornhill Newmarket Georgetown Pickering Courtice Ancaster
-
+		Ajax Ancaster Aurora Brampton Burlington Courtice Downsview Etobicoke Georgetown Hamilton Markham Milton
+	`	Mississauga Newmarket Oakville Oshawa Pickering Rexdale Scarborough Thornhill Toronto Vaughan Whitby
 		TORONTON BURLIGNTON GEOGRETOWN BRULINGTON MAKRHAM TOROTNO
 		),
 		# Can't qw() these:
-		'North York', 'Richmond Hill', 'Ricmond Hill'
+		'North York', 'Richmond Hill', 'RICMOND HILL'
 	],
 );
 
 # Grab ottawa towers
 my @ottawa_towers;
-foreach my $row (@rows) {
+foreach my $row (@{$data_rows}) {
 
 	my $where = $row->{Link_Station_Location} || '';
 
-	# Hack
+	# Hack... some stations have a coded location, so use their street address instead
 	if( $where =~ /\d/ ) {
 		($where) = $row->{Station_Location} =~ m/^(.*)\s+\(/;
 	}
@@ -106,7 +56,7 @@ foreach my $row (@rows) {
 	push @ottawa_towers, $row;
 }
 
-# Dump tower locations
+# Dump tower locations in west-east order
 my %seen_locations = ();
 foreach my $row (sort { $b->{Longitude} <=> $a->{Longitude} } @ottawa_towers ) {
 
@@ -115,6 +65,7 @@ foreach my $row (sort { $b->{Longitude} <=> $a->{Longitude} } @ottawa_towers ) {
 	$row->{Station_Location} =~ s/OTTAWA/Ottawa/;
 	$row->{Station_Location} =~ s/^(.*?)\s+\((.*?)\)\s*(.*?)?$/$2, $1, $3/;
 
+	# Stations may be licensed for multiple frequencies, but we don't care about that yet.
 	next if $seen_locations{ $row->{Station_Location} }++;
 
 	printf "% 40s %6dlat %6dlng, %dMHz, %dm AGL, %d dBW\n",
@@ -125,3 +76,88 @@ foreach my $row (sort { $b->{Longitude} <=> $a->{Longitude} } @ottawa_towers ) {
 		$row->{Tx_Antenna_Height_Above_Ground_Level},
 		$row->{Tx_Power};
 }
+
+sub retrieve_content
+{
+	my ($url) = @_;
+
+	# my $content = do { open(my $fh, "<raw_data.txt") or die "No data!";  local $/; <$fh> };
+	my $content = get( $url );
+	die "Couldn't fetch content" unless defined $content;
+
+	# Convert from DOS-format.
+	$content =~ s/\r\n/\n/g;
+
+	return $content;
+}
+
+# Return legend as an arrayref of hashrefs.  Each hashref contains:
+# 	name (from original legend, stripped of trailing spaces)
+# 	units (if determinable from name)
+# 	key (name stripped of unit information, whitespaces converted to _)
+# 	start (column index to start extraction)
+# 	end (column count)
+sub extract_legend
+{
+	my ($content) = @_;
+
+	my ($raw_legend) = $content =~ m/Field Position Legend(.*)/sm;
+	my $legend = [];
+	foreach my $line (split(/\n/, $raw_legend)) {
+
+		# Lines are in the format of:
+		# 	name    start - end
+		# with the columns starting at 1.
+
+		my ($name, $start, $end) = $line =~ m/(.*?)\s+(\d+) - (\d+)/;
+		next unless $name;
+		$name =~ s/\s+$//;
+
+		# Pull off units
+		my $units = undef;
+		if( $name =~ m/\((.*?)\)$/ ) {
+			$units = $1;
+		}
+
+		my $key = $name;
+		$key =~ s/\(.*?\)//g;
+		$key =~ s/\s+$//;
+		$key =~ s/\s+/_/g;
+
+		my $col = {
+			key   => $key,
+			units => $units,
+			name  => $name,
+			start => $start - 1,
+			len   => $end - $start + 1,
+		};
+		push @$legend,$col;
+
+	}
+
+	return $legend;
+}
+
+# Return data as an arrayref of hashrefs, one per row.
+sub extract_data
+{
+	my ($legend, $content) = @_;
+
+	my $regex   = join('\s', map { "(.{$_->{len},$_->{len}})" } @$legend );
+	my @key_ary = map { $_->{key} } @$legend;
+
+	my ($data)   = $content =~ m/\[DATA\](.*)\[\/DATA\]/sm;
+	my @rows;
+	foreach my $line (split(/\n/,$data)) {
+		my (@tmprow) = $line =~ /$regex/o;
+
+		my %row;
+
+		@tmprow = map { s/^\s+//; $_ } @tmprow;
+		@row{@key_ary} = map { s/\s+$//; $_ } @tmprow;
+		push @rows, \%row;
+	}
+
+	return \@rows;
+}
+
